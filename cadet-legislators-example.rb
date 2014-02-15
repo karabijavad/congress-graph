31149af7692d2d9f2a4703b1c87e9c721d750bbb
Javad Karabi <karabijavad@gmail.com>
2014-02-15 17:29:49 -0500

    just use get_node, instead of going through method_missing, for performance improvement

--- cadet-legislators-example.rb
+++ cadet-legislators-example.rb
@@ -17,28 +17,28 @@ end
 
 YAML.load_file('data/congress-legislators/legislators-current.yaml').each do |leg|
   db.transaction do
-    l = db.get_a_Legislator_by_thomas_id leg["id"]["thomas"].to_i
+    l = db.get_node "Legislator", "thomas_id", leg["id"]["thomas"].to_i
 
     if leg["bio"]["gender"]
-      gender = db.get_a_Gender_by_name(leg["bio"]["gender"])
+      gender = db.get_node "Gender", "name", leg["bio"]["gender"]
       l.outgoing("gender") << gender
     end
 
     if leg["bio"]["religion"]
-      religion = db.get_a_Religion_by_name(leg["bio"]["religion"])
+      religion = db.get_node "Religion", "name", leg["bio"]["religion"]
       l.outgoing("religion") << religion
     end
 
     leg["terms"].each do |term|
       t = db.create_node("Term", "start", term["start"][0...4].to_i)
 
-      party = db.get_a_Party_by_name(term["party"])
+      party = db.get_node "Party", "name", term["party"]
       t.outgoing("party")      << party
 
-      state = db.get_a_State_by_name(term["state"])
+      state = db.get_node "State", "name", term["state"]
       t.outgoing("represents") << state
 
-      role = db.get_a_Role_by_name(term["type"])
+      role = db.get_node "Role", "name", term["type"]
       t.outgoing("role")       << role
 
       l.outgoing("term") << t
@@ -51,17 +51,17 @@ Dir['data/congress-data/*/bills/*/*/*.json'].each do |json_file|
   bill_data = JSON.parse(File.read(json_file))
   db.transaction do |tx|
     begin
-      bill = db.get_a_Bill_by_id bill_data["bill_id"]
+      bill = db.get_node "Bill", "id", bill_data["bill_id"]
       bill["official_title"] = bill_data["official_title"].to_s
 
-      sponsor = db.get_a_Legislator_by_thomas_id bill_data["sponsor"]["thomas_id"].to_i
+      sponsor = db.get_node "Legislator", "thomas_id", bill_data["sponsor"]["thomas_id"].to_i
       bill.outgoing("sponsor") << sponsor
 
-      congress = db.get_a_Congress_by_number bill_data["congress"].to_i
+      congress = db.get_node "Congress", "number", bill_data["congress"].to_i
       bill.outgoing("congress") << congress
 
       bill_data["cosponsors"].each do |cosponsor|
-        cosponsor = db.get_a_Legislator_by_thomas_id cosponsor["thomas_id"].to_i
+        cosponsor = db.get_node "Legislator", "thomas_id", cosponsor["thomas_id"].to_i
         bill.outgoing("cosponsor") << cosponsor
       end
     rescue Exception => e
