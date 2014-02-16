0b0205e811a8c273f191ba2a588e4ee3ecca9077
Michael Hunger <github@jexp.de>
2014-02-16 11:25:45 +0100

    update bootstrap.sh - use curl instead of wget

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 mkdir .bundle/
 bundle install --path .bundle/
 
-wget http://dist.neo4j.org/neo4j-community-2.0.1-unix.tar.gz
+curl -O http://dist.neo4j.org/neo4j-community-2.0.1-unix.tar.gz
 tar xf neo4j-community-2.0.1-unix.tar.gz
 rm neo4j-community-2.0.1-unix.tar.gz
 
