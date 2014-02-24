6322b1d5302f02f81dd14a8d8c1ec81820ac8bf4
Javad Karabi <karabijavad@gmail.com>
2014-02-24 16:40:15 -0600

    rename

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rm -rf neo4j-community-2.0.1/data/graph.db/* && bundle exec ruby $@ cadet-legislators-example.rb
+rm -rf neo4j-community-2.0.1/data/graph.db/* && bundle exec ruby $@ congress-graph.rb
