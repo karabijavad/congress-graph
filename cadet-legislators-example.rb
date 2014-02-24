03dae89ad8bbf41758adae1677213a892b1a87d7
Javad Karabi <karabijavad@gmail.com>
2014-02-24 16:04:07 -0600

    update

--- cadet-legislators-example.rb
+++ cadet-legislators-example.rb
@@ -4,30 +4,28 @@ require 'json'
 
 db = Cadet::BatchInserter::Session.open("neo4j-community-2.0.1/data/graph.db")
 
-db.transaction do
-  db.constraint :Legislator, :name
-  db.constraint :Legislator, :thomas_id
-  db.constraint :Bill,       :id
-  db.constraint :Gender,     :name
-  db.constraint :Religion,   :name
-  db.constraint :Party,      :name
-  db.constraint :State,      :name
-  db.constraint :Role,       :name
-end
+db.constraint :Legislator, :name
+db.constraint :Legislator, :thomas_id
+db.constraint :Bill,       :id
+db.constraint :Gender,     :name
+db.constraint :Religion,   :name
+db.constraint :Party,      :name
+db.constraint :State,      :name
+db.constraint :Role,       :name
+db.constraint :District,   :district
 
 puts "loading committees"
 YAML.load_file('data/congress-legislators/committee-membership-current.yaml').each do |committee_data|
-  c = db.get_node :Committee, :thomas_id, committee_data[0].to_s
+  c = db.get_node(:Committee, :thomas_id, committee_data[0].to_s)
 
   committee_data[1].each do |leg|
-    l = db.get_node :Legislator, :thomas_id, leg["thomas"].to_i
+    l = db.get_node(:Legislator, :thomas_id, leg["thomas"].to_i)
     l.outgoing(:member_of_committee) << c
   end
 end
 
 puts "loading legislators"
 YAML.load_file('data/congress-legislators/legislators-current.yaml').each do |leg|
-  db.transaction do
     l = db.get_node(:Legislator, :thomas_id, leg["id"]["thomas"].to_i)
 
     if leg["bio"]["gender"]
@@ -41,21 +39,23 @@ YAML.load_file('data/congress-legislators/legislators-current.yaml').each do |le
     end
 
     leg["terms"].each do |term|
-      t = db.create_node(:Term, :start, term["start"][0...4].to_i)
+      t = db.create_node_with(:Term, {:start => term["start"].gsub(/-/, '').to_i, :end => term["end"].gsub(/-/, '').to_i})
 
       party = db.get_node(:Party, :name, term["party"])
       t.outgoing(:party)      << party
 
-      state = db.get_node(:State, :name, term["state"])
-      t.outgoing(:represents) << state
+      district = db.get_node(:District, :district, term["district"].to_i)
+      state    = db.get_node(:State, :name, term["state"])
+      district.outgoing(:state) << state
+
+      t.outgoing(:represents) << district
+      district.outgoing(:state) << db.get_node(:State, :name, term["state"])
 
       role = db.get_node(:Role, :name, term["type"])
       t.outgoing(:role)       << role
 
       l.outgoing(:term) << t
     end
-
-  end
 end
 
 puts "loading bills"
@@ -78,25 +78,23 @@ end
 until file_queue.empty? && data_queue.empty?
   bill_data = data_queue.pop
 
-  db.transaction do |tx|
     begin
-      bill = db.get_node :Bill, :id, bill_data["bill_id"]
+      bill = db.get_node(:Bill, :id, bill_data["bill_id"])
       bill["official_title"] = bill_data["official_title"].to_s
 
-      sponsor = db.get_node :Legislator, :thomas_id, bill_data["sponsor"]["thomas_id"].to_i
+      sponsor = db.get_node(:Legislator, :thomas_id, bill_data["sponsor"]["thomas_id"].to_i)
       bill.outgoing(:sponsor) << sponsor
 
-      congress = db.get_node :Congress, :number, bill_data["congress"].to_i
+      congress = db.get_node(:Congress, :number, bill_data["congress"].to_i)
       bill.outgoing(:congress) << congress
 
       bill_data["cosponsors"].each do |cosponsor|
-        cosponsor = db.get_node :Legislator, :thomas_id, cosponsor["thomas_id"].to_i
+        cosponsor = db.get_node(:Legislator, :thomas_id, cosponsor["thomas_id"].to_i)
         bill.outgoing(:cosponsor) << cosponsor
       end
     rescue Exception => e
       # tx.failure
     end
-  end
 end
 
 db.close
